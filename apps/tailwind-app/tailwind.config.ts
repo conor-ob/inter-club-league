6eb9f57abd14ad9b054fd31e0652e3a9ba850c59
conor-ob <conor.w.ob@gmail.com>
2024-02-28 22:27:06 +0000

    start nativewind v4 setup

---
+++ apps/tailwind-app/tailwind.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {}
+  },
+  future: {
+    hoverOnlyWhenSupported: true
+  },
+  plugins: []
+} satisfies Config
