e3569a06eccea5a2e8af08f0e919cbccc37962c5
conor-ob <conor.w.ob@gmail.com>
2024-03-11 00:11:15 +0000

    gc updates

--- apps/mobile/src/features/gc/gc-feature.tsx
+++ apps/mobile/src/features/gc/gc-feature.tsx
@@ -7,7 +7,6 @@ import { useGcQuery } from '@/graphql/use-gc-query'
 import { stageNumberFromStageId } from '@inter-club-league/utils'
 import cx from 'classnames'
 import { useLocalSearchParams } from 'expo-router'
-import { useCallback, useEffect, useState } from 'react'
 import { FlatList, Platform, Text, View } from 'react-native'
 import { GcHeader } from './gc-header'
 import { GcRiderComponent } from './gc-rider-component'
@@ -15,18 +14,6 @@ import { GcRiderComponent } from './gc-rider-component'
 export function GcFeature() {
   const { id } = useLocalSearchParams<{ id: string }>()
   const { data, loading, error, refetch } = useGcQuery(id)
-  const [refreshing, setRefreshing] = useState(false)
-
-  const handleRefresh = useCallback(() => {
-    setRefreshing(true)
-    refetch()
-  }, [refetch])
-
-  useEffect(() => {
-    if (!loading) {
-      setRefreshing(false)
-    }
-  }, [loading])
 
   return (
     <RefreshScrollView loading={loading} onRefetch={() => refetch()}>
@@ -83,55 +70,4 @@ export function GcFeature() {
       )}
     </RefreshScrollView>
   )
-
-  // return (
-  //   <FlatList
-  //     data={
-  //       loading
-  //         ? [undefined, undefined, undefined, undefined, undefined]
-  //         : data?.gc.gcRiders
-  //     }
-  //     contentInsetAdjustmentBehavior='automatic'
-  //     refreshControl={
-  //       <RefreshControl
-  //         refreshing={refreshing}
-  //         onRefresh={() => {
-  //           handleRefresh()
-  //         }}
-  //       />
-  //     }
-  //     ItemSeparatorComponent={() => <CardDivider />}
-  //     keyExtractor={(item, index) =>
-  //       loading
-  //         ? `loading-${index.toString()}`
-  //         : data
-  //           ? item.id
-  //           : `error-${index.toString()}`
-  //     }
-  //     renderItem={({ item }) => {
-  //       return loading ? (
-  //         <View
-  //           className={cx('py-8', {
-  //             'px-4': Platform.OS === 'android',
-  //             'px-5': Platform.OS === 'ios'
-  //           })}
-  //         >
-  //           <Skeleton className='h-4' />
-  //         </View>
-  //       ) : data ? (
-  //         <GcRiderComponent key={item.id} gcRider={item} />
-  //       ) : (
-  //         <View>
-  //           <Card>
-  //             <Text className='text-primary px-5 py-6'>
-  //               {JSON.stringify(error, null, 2)}
-  //             </Text>
-  //           </Card>
-  //         </View>
-  //       )
-  //     }}
-  //     ListHeaderComponent={() => <GcHeader />}
-  //     stickyHeaderIndices={[0]}
-  //   />
-  // )
 }
