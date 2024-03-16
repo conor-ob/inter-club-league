adfc04166c413bf18e8b1363da503ef20ef44960
conor-ob <conor.w.ob@gmail.com>
2024-03-16 02:32:35 +0000

    yellow jersey

--- packages/app/components/image/yellow-jersey.tsx
+++ packages/app/components/image/yellow-jersey.tsx
@@ -1,5 +1,12 @@
 import { SolitoImage } from 'solito/image'
 
 export function YellowJersey() {
-  return <SolitoImage src='/yellow-jersey.png' />
+  return (
+    <SolitoImage
+      src='/yellow-jersey.png'
+      width={100}
+      height={100}
+      alt='Yellow Jersey'
+    />
+  )
 }
