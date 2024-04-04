0ce9f71741872d99906528e43ba0b44430b49adc
conor-ob <conor.w.ob@gmail.com>
2024-04-04 14:30:02 +0100

    jersey size

--- packages/app/components/image/yellow-jersey.tsx
+++ packages/app/components/image/yellow-jersey.tsx
@@ -1,10 +1,9 @@
-import cx from 'classnames'
 import { Image } from 'react-native'
 
 export function YellowJersey({ className }: { className?: string }) {
   return (
     <Image
-      className={cx('h-8 w-8', className)}
+      className={className ?? 'h-8 w-8'}
       source={{
         uri: 'https://cdn.jsdelivr.net/gh/conor-ob/cdn@latest/image/inter-club-league/yellow-jersey.png'
       }}
