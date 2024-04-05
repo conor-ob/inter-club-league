aeca0281e59b5a56006e6281e748667d64616fdb
conor-ob <conor.w.ob@gmail.com>
2024-04-05 16:00:26 +0100

    part 3

--- packages/app/features/schedule/schedule-feature.tsx
+++ packages/app/features/schedule/schedule-feature.tsx
@@ -1,5 +1,6 @@
 import SegmentedControl from '@react-native-segmented-control/segmented-control'
-import { ScrollView, View, useColorScheme } from 'react-native'
+import { RefreshScrollView } from 'app/components/view/refresh-scroll-view'
+import { View, useColorScheme } from 'react-native'
 import { Skeleton } from '../../components/loading/skeleton'
 import { colors } from '../../design/colors'
 import { useStagesQuery } from '../../graphql/use-stages-query'
@@ -10,36 +11,40 @@ export function ScheduleFeature() {
   const colorScheme = useColorScheme()
   const { data, loading, error, refetch } = useStagesQuery(undefined)
 
-  if (data?.stages) {
-    return (
-      <ScrollView className='px-4 pb-6 pt-2'>
+  return (
+    <RefreshScrollView
+      contentContainerClassName='px-4 pb-6 pt-2'
+      loading={loading}
+      onRefresh={() => refetch()}
+    >
+      {loading ? (
+        <View>
+          <SegmentedControl
+            values={['Upcoming', 'Completed']}
+            enabled={false}
+            fontStyle={{
+              fontWeight: '500',
+              color: colors[colorScheme ?? 'light'].textColorSecondary
+            }}
+            activeFontStyle={{
+              fontWeight: '600',
+              color: colors[colorScheme ?? 'light'].textColorPrimary
+            }}
+          />
+          <View className='h-6' />
+          <Skeleton className='mx-3 h-5 w-12 rounded-md' />
+          <View className='h-3' />
+          <Skeleton className='h-40 rounded-xl' />
+          <View className='h-6' />
+          <Skeleton className='mx-3 h-5 w-12 rounded-md' />
+          <View className='h-3' />
+          <Skeleton className='h-80 rounded-xl' />
+        </View>
+      ) : data ? (
         <ScheduleList schedule={buildSchedule(data?.stages ?? [])} />
-      </ScrollView>
-    )
-  } else {
-    return (
-      <ScrollView className='px-4 pb-6 pt-2'>
-        <SegmentedControl
-          values={['Upcoming', 'Completed']}
-          enabled={false}
-          fontStyle={{
-            fontWeight: '500',
-            color: colors[colorScheme ?? 'light'].textColorSecondary
-          }}
-          activeFontStyle={{
-            fontWeight: '600',
-            color: colors[colorScheme ?? 'light'].textColorPrimary
-          }}
-        />
-        <View className='h-6' />
-        <Skeleton className='mx-3 h-5 w-12 rounded-md' />
-        <View className='h-3' />
-        <Skeleton className='h-40 rounded-xl' />
-        <View className='h-6' />
-        <Skeleton className='mx-3 h-5 w-12 rounded-md' />
-        <View className='h-3' />
-        <Skeleton className='h-80 rounded-xl' />
-      </ScrollView>
-    )
-  }
+      ) : (
+        <View />
+      )}
+    </RefreshScrollView>
+  )
 }
