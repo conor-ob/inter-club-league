401789cfc83db054c0d03bbe74791db3e792140f
conor-ob <conor.w.ob@gmail.com>
2024-04-08 23:09:51 +0100

    hide crap from map

--- packages/app/features/stage/components/stage-map/stage-map.web.tsx
+++ packages/app/features/stage/components/stage-map/stage-map.web.tsx
@@ -4,11 +4,16 @@ import { Column } from 'app/components/layout/column'
 import { Row } from 'app/components/layout/row'
 import { colors } from 'app/design/colors'
 import { Stage } from 'app/generated/graphql'
+import { useEffect, useState } from 'react'
 import { Text, TouchableOpacity, View, useColorScheme } from 'react-native'
 import { Link } from 'solito/link'
 
 export function StageMap({ stage }: { stage: Stage }) {
   const colorScheme = useColorScheme()
+
+  const screenSize = useScreenSize()
+  const mapWidth = screenSize.width - 32 // hack for padding of px-4 on each side - 16 + 16 =  32px
+  const marginLeft = `-${Math.floor(mapWidth / 2)}px`
 
   const apiKey = 'AIzaSyAcdXzRjKbuTL6DCbjR9r74lAmZhppaW_M'
   const zoom = 14
@@ -21,12 +26,23 @@ export function StageMap({ stage }: { stage: Stage }) {
       <View className='h-2' />
       <Card>
         <Column>
-          <iframe
-            className='h-80 rounded-t-xl focus:border-0'
-            style={{ border: '0' }}
-            loading='lazy'
-            src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${stage.coordinates}&center=${stage.coordinates}&zoom=${zoom}`}
-          />
+          <div
+            className='rounded-t-xl'
+            style={{ width: '100%', overflow: 'hidden', height: '250px' }}
+          >
+            <iframe
+              style={{
+                border: '0',
+                marginTop: '-125px',
+                marginLeft: marginLeft,
+                pointerEvents: 'none'
+              }}
+              width='200%'
+              height='500'
+              loading='lazy'
+              src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${stage.coordinates}&center=${stage.coordinates}&zoom=${zoom}`}
+            />
+          </div>
           <Row className='items-center justify-between px-4 py-4'>
             <Text className='text-secondary font-inter-regular text-base'>
               {stage.location}
@@ -69,4 +85,29 @@ function isAppleMobile(): boolean {
   )
     return true
   return false
+}
+
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  })
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return screenSize
 }
