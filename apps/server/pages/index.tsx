31cee8d2713c90e67334a324399051018cb55359
conor-ob <conor.w.ob@gmail.com>
2024-04-09 02:22:50 +0100

    dont redirect from base url

--- apps/server/pages/index.tsx
+++ apps/server/pages/index.tsx
@@ -1,3 +1,23 @@
-import GcRedirect from './gc/index'
+import { GcFeature } from 'app/features/gc/gc-feature'
+import { GcFeatureSkeleton } from 'app/features/gc/gc-feature-skeleton'
+import { useRedirectQuery } from 'app/graphql/use-redirect-query'
+import { ScrollView } from 'react-native'
 
-export default GcRedirect
+export default function Index() {
+  const { loading, data, error, refetch } = useRedirectQuery({
+    seasonId: undefined
+  })
+
+  if (data?.redirects.currentStageId) {
+    return <GcFeature providedStageId={data.redirects.currentStageId} />
+  } else {
+    return (
+      <ScrollView
+        contentContainerClassName='px-4 py-6'
+        contentInsetAdjustmentBehavior='automatic'
+      >
+        <GcFeatureSkeleton />
+      </ScrollView>
+    )
+  }
+}
